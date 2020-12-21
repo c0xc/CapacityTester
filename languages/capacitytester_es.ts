a23cfe42e5f9196261f13e52af2e205f637d0425
Philip Seeger <philip@philip-seeger.de>
2020-12-21 02:13:28 +0100

    Add Spanish translation files

---
+++ languages/capacitytester_es.ts
@@ -0,0 +1,376 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>CapacityTesterCli</name>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="46"/>
+        <source>Lists available volumes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="48"/>
+        <source>Shows volume information.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="50"/>
+        <source>Starts volume test.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="52"/>
+        <source>Answers questions with yes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="54"/>
+        <source>Changes the size of the safety buffer zone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="57"/>
+        <source>Volume to be tested.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="226"/>
+        <source>Capacity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="231"/>
+        <source>Used:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="238"/>
+        <source>Available:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="252"/>
+        <source>%1 old test file(s) have been found.
+Cannot test with these files present, please delete them!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="268"/>
+        <source>%1 file(s) have been found.
+The volume should be completely empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="298"/>
+        <source>The selected volume is full.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="307"/>
+        <source>The volume is not empty: %1 file(s) have been found.
+You should delete all those files first.
+Are you really sure you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="445"/>
+        <source>Test completed successfully, no errors found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="454"/>
+        <source>The volume might be bad.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="455"/>
+        <source>An error has occurred.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="457"/>
+        <source>Error creating test file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="459"/>
+        <source>Permission denied.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="461"/>
+        <source>Resizing test file failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="463"/>
+        <source>Write failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="465"/>
+        <source>Verification failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="466"/>
+        <source>Test failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CapacityTesterGui</name>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="41"/>
+        <source>Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="46"/>
+        <source>Mounted volume to be tested</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="51"/>
+        <source>&amp;Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="74"/>
+        <source>Capacity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="80"/>
+        <source>Used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="86"/>
+        <source>Available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="138"/>
+        <source>Initializing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="140"/>
+        <source>Writing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="142"/>
+        <source>Verifying</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="148"/>
+        <source>Write speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="154"/>
+        <source>Read speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="166"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="172"/>
+        <source>Result</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="184"/>
+        <source>Start &amp;Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="188"/>
+        <source>&amp;Stop Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="192"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="261"/>
+        <source>Abort test?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="262"/>
+        <source>Do you want to abort the running test?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="413"/>
+        <location filename="../src/capacitytestergui.cpp" line="511"/>
+        <source>Volume invalid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="414"/>
+        <source>The selected volume is not valid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="448"/>
+        <source>Old test files found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="449"/>
+        <source>%1 old test file(s) have been found! Cannot test with these files present, please delete them! The volume should be completely empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="468"/>
+        <location filename="../src/capacitytestergui.cpp" line="538"/>
+        <source>Volume not empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="469"/>
+        <source>%1 file(s) have been found. The volume should be completely empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="512"/>
+        <source>The selected volume is not valid anymore.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="526"/>
+        <source>Volume full</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="527"/>
+        <source>The selected volume is full.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="539"/>
+        <source>The volume is not empty: %1 file(s) have been found. You should cancel, delete all those files and try again. Are you really sure you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="678"/>
+        <source>STARTING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="695"/>
+        <source>STOPPING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="711"/>
+        <source>TEST SUCCEEDED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="714"/>
+        <source>TEST COMPLETED SUCCESSFULLY, NO ERRORS FOUND.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="730"/>
+        <source>Test succeeded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="731"/>
+        <source>Test completed successfully, no errors found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="741"/>
+        <source>TEST FAILED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="762"/>
+        <source>The volume might be bad.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="763"/>
+        <source>An error has occurred.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="765"/>
+        <source>Error creating test file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="767"/>
+        <source>Permission denied.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="769"/>
+        <source>Resizing test file failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="771"/>
+        <source>Write failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="773"/>
+        <source>Verification failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="775"/>
+        <source>Test failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="776"/>
+        <source>Test failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="884"/>
+        <source>INITIALIZING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="903"/>
+        <source>WRITING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="913"/>
+        <source>VERIFYING</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="926"/>
+        <source>ACCESS ERROR!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="940"/>
+        <source>WRITE ERROR AFTER %1 MB!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="954"/>
+        <source>READ ERROR AFTER %1 MB!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

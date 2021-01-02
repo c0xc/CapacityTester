4b36a9b48cad8e94421901a80e1a4bd62112b7d8
Philip Seeger <philip@philip-seeger.de>
2021-01-02 21:13:10 +0100

    Add Russian translation

---
+++ languages/capacitytester_ru.ts
@@ -0,0 +1,380 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>CapacityTesterCli</name>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="46"/>
+        <source>Lists available volumes.</source>
+        <translation>Перечисляет доступные файловые системы.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="48"/>
+        <source>Shows volume information.</source>
+        <translation>Показывает информацию о файловых системах.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="50"/>
+        <source>Starts volume test.</source>
+        <translation>Начинает проверку файловых систем.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="52"/>
+        <source>Answers questions with yes.</source>
+        <translation>Положительно отвечает на все вопросы.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="54"/>
+        <source>Changes the size of the safety buffer zone.</source>
+        <translation>Изменяет размер свободной буферной зоны.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="57"/>
+        <source>Volume to be tested.</source>
+        <translation>Файловая система для проверки.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="226"/>
+        <source>Capacity:</source>
+        <translation>Ёмкость:</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="231"/>
+        <source>Used:</source>
+        <translation>Использовано:</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="238"/>
+        <source>Available:</source>
+        <translation>Свободно:</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="252"/>
+        <source>%1 old test file(s) have been found.
+Cannot test with these files present, please delete them!</source>
+        <translation>Обнаружены старые тестовые файлы: %1
+Они мешают дальнейшей проверке. Пожалуйста, удалите их!</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="268"/>
+        <source>%1 file(s) have been found.
+The volume should be completely empty.</source>
+        <translation>Обнаружены файлы: %1
+Файловая система должна быть абсолютно пустой.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="298"/>
+        <source>The selected volume is full.</source>
+        <translation>Выбранная файловая система заполнена.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="307"/>
+        <source>The volume is not empty: %1 file(s) have been found.
+You should delete all those files first.
+Are you really sure you want to continue?</source>
+        <translation>Файловая система не пуста. Обнаружены файлы: %1
+Сначала удалите все эти файлы.
+Вы уверены, что Вы несмотря на это хотите продолжить?</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="445"/>
+        <source>Test completed successfully, no errors found.</source>
+        <translation>Проверка успешно завершена. Ошибок не найдено.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="454"/>
+        <source>The volume might be bad.</source>
+        <translation>Файловая система повреждена.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="455"/>
+        <source>An error has occurred.</source>
+        <translation>Возникла ошибка.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="457"/>
+        <source>Error creating test file.</source>
+        <translation>Возникла ошибка при создании тестового файла.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="459"/>
+        <source>Permission denied.</source>
+        <translation>Отказано в доступе.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="461"/>
+        <source>Resizing test file failed.</source>
+        <translation>Увеличение тестового файла не получилось.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="463"/>
+        <source>Write failed.</source>
+        <translation>Запись не получилась.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="465"/>
+        <source>Verification failed.</source>
+        <translation>Верификация не получилась.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestercli.cpp" line="466"/>
+        <source>Test failed.</source>
+        <translation>Проверка не получилась.</translation>
+    </message>
+</context>
+<context>
+    <name>CapacityTesterGui</name>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="41"/>
+        <source>Volume</source>
+        <translation>Файловая система</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="46"/>
+        <source>Mounted volume to be tested</source>
+        <translation>Файловая система для проверки</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="51"/>
+        <source>&amp;Refresh</source>
+        <translation>Об&amp;новить</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="74"/>
+        <source>Capacity</source>
+        <translation>Ёмкость</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="80"/>
+        <source>Used</source>
+        <translation>Использовано</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="86"/>
+        <source>Available</source>
+        <translation>Свободно</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="138"/>
+        <source>Initializing</source>
+        <translation>Подготовка</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="140"/>
+        <source>Writing</source>
+        <translation>Запись</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="142"/>
+        <source>Verifying</source>
+        <translation>Верификация</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="148"/>
+        <source>Write speed</source>
+        <translation>Скорость записи</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="154"/>
+        <source>Read speed</source>
+        <translation>Скорость чтения</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="166"/>
+        <source>Time</source>
+        <translation>Время</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="172"/>
+        <source>Result</source>
+        <translation>Результат</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="184"/>
+        <source>Start &amp;Test</source>
+        <translation>Начать &amp;проверку</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="188"/>
+        <source>&amp;Stop Test</source>
+        <translation>&amp;Закончить проверку</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="192"/>
+        <source>&amp;Quit</source>
+        <translation>Завер&amp;шить</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="261"/>
+        <source>Abort test?</source>
+        <translation>Прервать проверку?</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="262"/>
+        <source>Do you want to abort the running test?</source>
+        <translation>Вы уверены, что хотите прервать текущую проверку?</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="413"/>
+        <location filename="../src/capacitytestergui.cpp" line="511"/>
+        <source>Volume invalid</source>
+        <translation>Файловая система недействительна</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="414"/>
+        <source>The selected volume is not valid.</source>
+        <translation>Выбранная файловая система недействительна.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="448"/>
+        <source>Old test files found</source>
+        <translation>Обнаружены старые тестовые файлы</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="449"/>
+        <source>%1 old test file(s) have been found! Cannot test with these files present, please delete them! The volume should be completely empty.</source>
+        <translation>Обнаружены старые тестовые файлы: %1. Они мешают дальнейшей проверке. Пожалуйста, удалите их! Файловая система должна быть абсолютно пустой.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="468"/>
+        <location filename="../src/capacitytestergui.cpp" line="538"/>
+        <source>Volume not empty</source>
+        <translation>Файловая система не пуста</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="469"/>
+        <source>%1 file(s) have been found. The volume should be completely empty.</source>
+        <translation>Обнаружены файлы: %1. Файловая система должна быть абсолютно пустой.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="512"/>
+        <source>The selected volume is not valid anymore.</source>
+        <translation>Выбранная файловая система больше недействительна.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="526"/>
+        <source>Volume full</source>
+        <translation>Файловая система заполнена</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="527"/>
+        <source>The selected volume is full.</source>
+        <translation>Выбранная файловая система заполнена.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="539"/>
+        <source>The volume is not empty: %1 file(s) have been found. You should cancel, delete all those files and try again. Are you really sure you want to continue?</source>
+        <translation>Файловая система не пуста. Обнаружены файлы: %1. Удалите все эти файлы и попробуйте ещё раз. Вы уверены, что хотите продолжить?</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="678"/>
+        <source>STARTING</source>
+        <translation>ЗАПУСК</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="695"/>
+        <source>STOPPING</source>
+        <translation>ОСТАНОВКА</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="711"/>
+        <source>TEST SUCCEEDED</source>
+        <translation>ПРОВЕРКА ЗАВЕРШЕНА</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="714"/>
+        <source>TEST COMPLETED SUCCESSFULLY, NO ERRORS FOUND.</source>
+        <translation>ПРОВЕРКА УСПЕШНО ЗАВЕРШЕНА. ОШИБОК НЕ НАЙДЕНО.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="730"/>
+        <source>Test succeeded</source>
+        <translation>Проверка завершена</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="731"/>
+        <source>Test completed successfully, no errors found.</source>
+        <translation>Проверка успешно завершена. Ошибок не найдено.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="741"/>
+        <source>TEST FAILED</source>
+        <translation>ПРОВЕРКА НЕ ПОЛУЧИЛАСЬ</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="762"/>
+        <source>The volume might be bad.</source>
+        <translation>Файловая система повреждена.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="763"/>
+        <source>An error has occurred.</source>
+        <translation>Возникла ошибка.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="765"/>
+        <source>Error creating test file.</source>
+        <translation>Возникла ошибка при создании тестового файла.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="767"/>
+        <source>Permission denied.</source>
+        <translation>Отказано в доступе.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="769"/>
+        <source>Resizing test file failed.</source>
+        <translation>Увеличение тестового файла не получилось.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="771"/>
+        <source>Write failed.</source>
+        <translation>Запись не получилась.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="773"/>
+        <source>Verification failed.</source>
+        <translation>Верификация не получилась.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="776"/>
+        <source>Test failed.</source>
+        <translation>Проверка не получилась.</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="775"/>
+        <source>Test failed</source>
+        <translation>Проверка не получилась</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="884"/>
+        <source>INITIALIZING</source>
+        <translation>ПОДГОТОВКА</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="903"/>
+        <source>WRITING</source>
+        <translation>ЗАПИСЬ</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="913"/>
+        <source>VERIFYING</source>
+        <translation>ВЕРИФИКАЦИЯ</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="926"/>
+        <source>ACCESS ERROR!</source>
+        <translation>ОШИБКА ДОСТУПА!</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="940"/>
+        <source>WRITE ERROR AFTER %1 MB!</source>
+        <translation>ОШИБКА ЗАПИСИ ПОСЛЕ %1 MB!</translation>
+    </message>
+    <message>
+        <location filename="../src/capacitytestergui.cpp" line="954"/>
+        <source>READ ERROR AFTER %1 MB!</source>
+        <translation>ОШИБКА ЧТЕНИЯ ПОСЛЕ %1 MB!</translation>
+    </message>
+</context>
+</TS>
